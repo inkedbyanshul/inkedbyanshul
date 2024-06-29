3338af27d9978f6ae3ec93dc90e1998c790b617d
inkedbyanshul <inkedbyanshul@gmail.com>
2024-06-29 19:27:41 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,19 @@
-'use strict';
+function emailSend(){
 
 
 
-/**
- * add event on elements
- */
-
-const addEventOnElem = function (elem, type, callback) {
-  if (elem.length > 1) {
-    for (let i = 0; i < elem.length; i++) {
-      elem[i].addEventListener(type, callback);
-    }
-  } else {
-    elem.addEventListener(type, callback);
-  }
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "inkedbyanshul@gmail.com
+",
+    Password : "E5C253E66645A4FC6268C139EC598D3AF5DC
+",
+    To : 'anshularya512@gmail.com',
+    From : "inkedbyanshul@gmail.com
+",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
 }
-
-
-
-/**
- * navbar toogle
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const overlay = document.querySelector("[data-overlay]");
-
-const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
-
-addEventOnElem(navTogglers, "click", toggleNavbar);
-
-
-
-/**
- * active header & back top btn when window scroll down to 100px
- */
-
-const header = document.querySelector("[data-header]");
-const backTopBtn = document.querySelector("[data-back-top-btn]");
-
-const activeElemOnScroll = function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-}
-
-addEventOnElem(window, "scroll", activeElemOnScroll);
-
-
-// check input for value; float label
-
-var inputs = document.querySelectorAll('input[type="text"]');
-var form = document.getElementById('address-form');
-
-function labelUp(input) {
-  var _this = input;
-  if (_this.classList.contains('open')) {
-    return;
-  }
-  _this.classList.add('open');
-}
-
-function labelDown(input) {
-  var _this = input;
-  if (_this.classList.contains('open') && !_this.value) {
-    _this.classList.remove('open');
-  }
-}
-
-form.addEventListener('input', function(e) {
-  if (e.target.tagName === "INPUT") {
-    labelUp(e.target);
-  }
-}, false);
-
-for (var i = 0, l = inputs.length; i < l; i++) {
-  inputs[i].addEventListener('blur', function(e) {
-      labelDown(e.target);
-  }, false);
-}
-/**
- * filter functionality
- */
-
-const filterBtn = document.querySelectorAll("[data-filter-btn]");
-const filterItems = document.querySelectorAll("[data-filter]");
-
-let lastClickedBtn = filterBtn[0];
-
-const filter = function () {
-  lastClickedBtn.classList.remove("active");
-  this.classList.add("active");
-  lastClickedBtn = this;
-
-  for (let i = 0; i < filterItems.length; i++) {
-    if (filterItems[i].dataset.filter === this.dataset.filterBtn) {
-      filterItems[i].style.display = "block";
-    } else {
-      filterItems[i].style.display = "none";
-    }
-  }
-}
-
-addEventOnElem(filterBtn, "click", filter);
