f6fbe4e07396d9c89a16fea4ae4c93798d9cb3e2
inkedbyanshul <inkedbyanshul@gmail.com>
2024-06-29 21:11:29 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,30 @@
+
+
+
 function emailSend(){
 
-
-var userName = document.getElementById('name').value;
+	var userName = document.getElementById('name').value;
 	var phone = document.getElementById('phone').value;
 	var email = document.getElementById('email').value;
 
 	var messageBody = "Name " + userName +
 	"<br/> Phone " + phone +
 	"<br/> Email " + email;
-  Email.send({
+	Email.send({
     Host : "smtp.elasticemail.com",
-    Username : "inkedbyanshul@gmail.com
-",
-    Password : "E5C253E66645A4FC6268C139EC598D3AF5DC
-",
+    Username : "inkedbyanshul@gmail.com",
+    Password : "E5C253E66645A4FC6268C139EC598D3AF5DC",
     To : 'anshularya512@gmail.com',
-    From : "inkedbyanshul@gmail.com
-",
+    From : "inkedbyanshul@gmail.com",
     Subject : "This is the subject",
-    Body : "And this is the body"
+    Body : messageBody
 }).then(
-  message => alert(message)
+  message => {
+  	if(message=='OK'){
+  		swal("Secussful", "You clicked the button!", "success");
+  	}
+  	else{
+  		swal("Error", "You clicked the button!", "error");
+  	}
+  }
 );
-}
